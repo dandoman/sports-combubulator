403dd122d2bab82c3b2dfc890fcea7217657c83f
Dando <davide.cannucci@gmail.com>
2016-10-04 05:45:36 -0400

    Modified front end such that we dont have to reload whole page

--- src/main/resources/static/js/nfl.js
+++ src/main/resources/static/js/nfl.js
@@ -2,7 +2,6 @@ $(document).ready(function(){
     $.get( "/api/match", { league: "NFL"} )
   		.done(function( data ) {
     		data.forEach(function(arrayItem){
-    			console.log(arrayItem);
     			$("#matches").append(`<div id="${arrayItem.match.matchId}"><p><strong>${arrayItem.match.visitorTeamName} @ ${arrayItem.match.homeTeamName}</strong>
     			<br>Kickoff: ${new Date(arrayItem.match.matchStartTime).toLocaleString()}
     			<br>${arrayItem.match.visitorTeamName} - Votes to win: <strong>${arrayItem.countAwayWins}</strong> Median predicted score: <strong>${arrayItem.medianAwayScore}</strong>
@@ -36,7 +35,18 @@ function createPrediction(matchId) {
     	}),
     	contentType: 'application/json',
     	success: function (data) {
-        	location.reload(true);
+        	var url = "/api/match/" + matchId;
+    		$.get(url) 
+    			.done(function( arrayItem ) {
+    				$("#matches").find('#' + matchId).find("p")
+    				.replaceWith(`<p><strong>${arrayItem.match.visitorTeamName} @ ${arrayItem.match.homeTeamName}</strong>
+    				<br>Kickoff: ${new Date(arrayItem.match.matchStartTime).toLocaleString()}
+    				<br>${arrayItem.match.visitorTeamName} - Votes to win: <strong>${arrayItem.countAwayWins}</strong> Median predicted score: <strong>${arrayItem.medianAwayScore}</strong>
+    				<br>${arrayItem.match.homeTeamName} -  Votes to win: <strong>${arrayItem.countHomeWins}</strong> Median predicted score: <strong>${arrayItem.medianHomeScore}</strong>
+    				<br>Your prediction - ${arrayItem.match.visitorTeamName}: <input size="2" type="text" class="visitorScorePrediction" value="${arrayItem.predictedVisitorScore != null ? arrayItem.predictedVisitorScore : ""}"> ${arrayItem.match.homeTeamName}: <input size="2" type="text" class="homeScorePrediction" value="${arrayItem.predictedHomeScore != null ? arrayItem.predictedHomeScore : ""}">
+    				<br><button onClick="createOrUpdatePrediction('${arrayItem.match.matchId}', ${arrayItem.predictedWinnerId != null})">${arrayItem.predictedWinnerId != null ? "Update Prediction" : "Submit Prediction"}</button></p>`)
+    			}
+  			);
     	},
     	error: function (xhr, ajaxOptions, thrownError) {
 			alert(xhr.responseJSON.message);
@@ -58,7 +68,18 @@ function updatePrediction(matchId) {
     	}),
     	contentType: 'application/json',
     	success: function (data) {
-        	location.reload(true);
+    		var url = "/api/match/" + matchId;
+    		$.get(url) 
+    			.done(function( arrayItem ) {
+    				$("#matches").find('#' + matchId).find("p")
+    				.replaceWith(`<p><strong>${arrayItem.match.visitorTeamName} @ ${arrayItem.match.homeTeamName}</strong>
+    				<br>Kickoff: ${new Date(arrayItem.match.matchStartTime).toLocaleString()}
+    				<br>${arrayItem.match.visitorTeamName} - Votes to win: <strong>${arrayItem.countAwayWins}</strong> Median predicted score: <strong>${arrayItem.medianAwayScore}</strong>
+    				<br>${arrayItem.match.homeTeamName} -  Votes to win: <strong>${arrayItem.countHomeWins}</strong> Median predicted score: <strong>${arrayItem.medianHomeScore}</strong>
+    				<br>Your prediction - ${arrayItem.match.visitorTeamName}: <input size="2" type="text" class="visitorScorePrediction" value="${arrayItem.predictedVisitorScore != null ? arrayItem.predictedVisitorScore : ""}"> ${arrayItem.match.homeTeamName}: <input size="2" type="text" class="homeScorePrediction" value="${arrayItem.predictedHomeScore != null ? arrayItem.predictedHomeScore : ""}">
+    				<br><button onClick="createOrUpdatePrediction('${arrayItem.match.matchId}', ${arrayItem.predictedWinnerId != null})">${arrayItem.predictedWinnerId != null ? "Update Prediction" : "Submit Prediction"}</button></p>`)
+    			}
+  			);
     	},
     	error: function (xhr, ajaxOptions, thrownError) {
 			alert(xhr.responseJSON.message);
